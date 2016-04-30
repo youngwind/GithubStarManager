8f7275a09a2a4939a982babf5630bfb9b25ed80a
youngwind <540506594@qq.com>
2016-04-30 19:50:50 +0800

    添加分组接口

--- backend/routes/user_group.js
+++ backend/routes/user_group.js
@@ -10,7 +10,7 @@ router.get('/group/all', function (req, res, next) {
 });
 
 // 用户添加分组
-router.post('/:userName/add', function (req, res, next) {
+router.post('/:userName/save', function (req, res, next) {
   let {userName} = req.params;
   let {groupName} = req.body;
 
@@ -38,7 +38,44 @@ router.post('/:userName/add', function (req, res, next) {
 
     res.send({
       code: 0,
-      data: ret._id  // 返回分组的id
+      data: groupName  // 返回分组的名称
+    })
+
+  })()
+
+});
+
+// 用户删除分组
+router.post('/:userName/remove', function (req, res, next) {
+  let {userName} = req.params;
+  let {groupName} = req.body;
+
+  if (!groupName) throw new Error('分组名称不能为空');
+
+  (async()=> {
+
+    let ret = await userGroup.findOne({
+      userName,
+      groupName
+    });
+
+
+    if (!ret) {
+      res.send({
+        code: 1,
+        msg: '分组名称不存在,删除失败'
+      });
+      return;
+    }
+
+    let result = await userGroup.remove({
+      userName,
+      groupName
+    });
+
+    res.send({
+      code: 0,
+      data: true
     })
 
   })()
